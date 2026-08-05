1de5e821a62ec2d8657cecec06e5d29c7a48e828
gabriel aquino <kaiserhzx@gmail.com>
2026-08-05 14:47:58 -0300

    update_modulo03

---
+++ modulo03/logica_programação_gabrielaquino-ativ01.py
@@ -0,0 +1,18 @@
+python
+# Entrada de dados
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+# Cálculos
+soma = num1 + num2
+diferenca = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+resto = num1 % num2
+
+# Exibição dos resultados
+print(f"Soma: {soma}")
+print(f"Diferença: {diferenca}")
+print(f"Multiplicação: {multiplicacao}")
+print(f"Divisão: {divisao}")
+print(f"Resto da divisão (%): {resto}")
---
+++ modulo03/logica_programação_gabrielaquino-ativ03.py
@@ -0,0 +1,13 @@
+python
+idade = int(input("Digite a sua idade: "))
+
+if idade < 0:
+    print("Idade inválida!")
+elif idade <= 12:
+    print("Categoria: Criança")
+elif idade <= 17:
+    print("Categoria: Adolescente")
+elif idade <= 59:
+    print("Categoria: Adulto")
+else:
+    print("Categoria: Idoso")
